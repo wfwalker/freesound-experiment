7988c512342c07e4a236a3b8623d7541830f200b
Bill Walker <walker@shout.net>
2016-11-03 19:52:49 -0700

    remove commented-out code

--- client/app.js
+++ client/app.js
@@ -8,123 +8,7 @@ window.AudioContext = window.AudioContext||window.webkitAudioContext;
 
 var theCollection = new FreesoundCollection(new AudioContext());
 
-// var gAudioContext = new AudioContext();
-// var gSoundInfoByID = {};
-// var gBufferByID = {};
-// var gBufferSourceByID = {};
-// var gSearchHistory = {};
 var gTemplates = {};
-
-// find AudioBuffer by ID, create a BufferSource, start it playing
-// track all currently playing buffersources in gBufferSourceByID
-// don't start another copy of a sound if it is already playing
-
-// function playRandomBuffer() {
-//     console.log('playRandomBuffer');
-//     var soundIDs = Object.keys(gBufferByID);
-//     var randomIndex = Math.floor(Math.random() * soundIDs.length);
-//     playBufferForID(soundIDs[randomIndex]);
-// }
-
-// function playBufferForID(inID) {
-//     if (gBufferSourceByID[inID]) {
-//         console.log('ALREADY PLAYING', inID, gSoundInfoByID[inID].name);
-//         return;
-//     }
-
-//     console.log('PLAY', inID, gSoundInfoByID[inID].name);
-//     gSoundInfoByID[inID].starttime = new Date();
-
-//     // CREATE gain node
-//     var gainNode = gAudioContext.createGain();
-//     gainNode.gain.value = 1;
-//     gainNode.connect(gAudioContext.destination);
-
-//     // CREATE audio buffer source
-//     var aBufferSource = gAudioContext.createBufferSource();
-//     aBufferSource.buffer = gBufferByID[inID];
-//     aBufferSource.freesoundID = inID;
-//     aBufferSource.connect(gainNode);
-//     aBufferSource.start();
-//     aBufferSource.myGainNode = gainNode;
-
-//     gBufferSourceByID[inID] = aBufferSource;
-//     handleBufferSourceListUpdated(inID);
-
-//     $('#play-sound-' + inID).attr('playing', 'true');
-
-//     aBufferSource.addEventListener('ended', handleBufferPlayEnded);
-// }
-
-// function handleBufferPlayEnded(e) {
-//     var freesoundID = e.target.freesoundID;
-
-//     if (gSoundInfoByID[freesoundID]) {
-//         console.log('ENDED', gSoundInfoByID[freesoundID].name);
-//         // turn off timer
-//         delete gSoundInfoByID[freesoundID].starttime;
-//     } else {
-//         console.log('ENDED no sound info?', freesoundID);
-//     }
-
-//     // disconnect gain node
-//     gBufferSourceByID[freesoundID].myGainNode.disconnect();
-//     // remove buffer from global buffer list
-//     delete gBufferSourceByID[freesoundID];
-//     // notify observers of global buffer list
-//     handleBufferSourceListUpdated(freesoundID);
-
-//     // if there's a play-button, mark it as not playing
-//     var selectorString = '#play-sound-' + freesoundID;
-//     if ($(selectorString)) {
-//         $(selectorString).removeAttr('playing');
-//     } else {
-//         console.log('no play button');
-//     }
-// }
-
-// Make an XMLHttpRequest for the MP3 preview file, decode the MP3, and save the buffer
-
-// function handleSoundDownloadProgress(event) {
-//     var freesoundID = event.target.freesoundID;
-//     $('#play-sound-' + freesoundID).attr('loading', 'true');
-//     $('span[data-sound-id="' + freesoundID + '"]').text(Math.round(100 * event.loaded / event.total) + '%');
-// }
-
-// function handleSoundDownloadDone(event) {
-//     var freesoundID = event.target.freesoundID;
-//     $('#play-sound-' + freesoundID).removeAttr('loading');
-
-//     var progressIndicator = $('span[data-sound-id="' + freesoundID + '"]');
-//     progressIndicator.remove();
-
-//     console.log('about to decode', freesoundID, gSoundInfoByID[freesoundID].name);
-//     gAudioContext.decodeAudioData(event.target.response, function(inBuffer) {
-//         console.log('decoded', freesoundID, gSoundInfoByID[freesoundID].name);
-//         gBufferByID[freesoundID] = inBuffer;
-//         $('#play-sound-' + freesoundID).removeAttr('disabled');
-//         $('.remove-sound[data-sound-id="'+freesoundID+'"]').removeAttr('disabled');
-//         handleBufferListUpdate(freesoundID);
-//     }, function (e) {
-//         console.log('error handler', e);
-//     });
-// }
-
-// function downloadBufferForID(inID, url) {
-//     console.log('downloadBufferForID', inID, gSoundInfoByID[inID].name);
-//     var request = new XMLHttpRequest();
-//     request.open('GET', url, true);
-//     request.responseType = 'arraybuffer';
-//     request.freesoundID = inID;
-
-//     // handle progress events
-//     request.addEventListener('progress', handleSoundDownloadProgress);
-
-//     // Decode asynchronously
-//     request.onload = handleSoundDownloadDone;
-
-//     request.send();
-// }
 
 // create a play button for the sound info
 
@@ -148,23 +32,6 @@ function displaySoundInfo(inSearchText, inInfo) {
     }.bind(this));
 }
 
-// load the sound info for a given sound ID
-
-// function getInfoAndLoadPreviewByID(inSearchText, inID) {
-//     console.log('getInfoAndLoadPreviewByID', inID);
-//     freesound.getSound(inID,
-//         function(sound) {
-//             gSoundInfoByID[inID] = sound;
-//             displaySoundInfo(inSearchText, sound);
-//             handleSoundInfoUpdate(inID);
-//             downloadBufferForID(inID, sound.previews['preview-hq-mp3']);
-//         },
-//         function(e) {
-//             console.log("Sound could not be retrieved", e);
-//         }
-//     );
-// }
-
 // respond to an update in the buffer list by updating the count
 
 function handleBufferListUpdate(inID) {
@@ -181,38 +48,6 @@ function handleBufferListUpdate(inID) {
         }
     }
 }
-
-// respond to an update in the sound info list by updating the count
-
-// function handleSoundInfoUpdate(inID) {
-//     if (gSoundInfoByID[inID]) {
-//         console.log('sound info adding', gSoundInfoByID[inID].name);
-//     } else {
-//         console.log('sound info removing', inID);
-//     }
-// // }
-
-// function handleBufferSourceListUpdated(inID) {
-//     if (theCollection.bufferSourceByID[inID]) { 
-//         console.log('buffer sources add', theCollection.soundInfoByID[inID].name);
-//         $('#playingcontainer').append(gTemplates['buffer-playing'](theCollection.soundInfoByID[inID]));
-
-//         // TODO wire up remove sound button
-//         $('.remove-sound[data-sound-id="'+inID+'"]').click(function(event) {
-//             console.log('remove-sound', event.target);
-//             // stop playing,  remove from arrays, notify observers
-//             deleteBufferForID(inID);
-//             // remove from table
-//             $('#sound-button-row-' + inID).remove();
-//         });
-
-//     } else {
-//         console.log('buffer sources remove', inID);
-//         if ($('div[data-sound-id="' + inID + '"]')) {
-//             $('div[data-sound-id="' + inID + '"]').remove();
-//         }
-//     }
-// }
 
 // handle the event handler by starting a Freesound text search
 
@@ -231,43 +66,7 @@ function doSearch(inString) {
 
     console.log('about to search', inString);
     theCollection.search(inString);
-    // freesound.textSearch(inString, {},
-    //     function(resultsObject) {
-    //         for (var index = 0; index < resultsObject.results.length; index++) {
-    //             var tempID = resultsObject.results[index].id;
-    //             gSearchHistory[inString].push(tempID);
-    //             getInfoAndLoadPreviewByID(inString, tempID);
-    //         }
-    //     }, function(err) {
-    //         console.log('textsearch err', err);
-    //     }
-    // );
 }
-
-// function deleteBufferForID(anID) {  
-//     console.log('deleteBufferForID', anID);
-//     // stop playing
-//     if (gBufferSourceByID[anID]) {
-//         gBufferSourceByID[anID].stop();
-//     }
-//     delete gBufferSourceByID[anID];
-//     delete gSoundInfoByID[anID];
-//     delete gBufferByID[anID];
-//     handleBufferSourceListUpdated(anID);
-//     handleSoundInfoUpdate(anID);
-//     handleBufferListUpdate(anID);
-// }
-
-// function deleteBuffersForSearch(inSearchTerm) {
-//     var searchHits = gSearchHistory[inSearchTerm];
-
-//     console.log('deleteBuffersForSearch', inSearchTerm, searchHits);
-
-//     for (var index = 0; index < searchHits.length; index++) {
-//         var anID = searchHits[index];
-//         deleteBufferForID(anID);
-//     }
-// }
 
 // handle search event by tokenizing the string and doing a separate search for each token
 
