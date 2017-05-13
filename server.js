1a3eee54b003b0b83e7dbfda4b31af3766710bbc
Bill Walker <walker@shout.net>
2017-05-13 08:29:49 -0700

    server can be even simplier

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 // invoke freesound api
 
 var express = require('express');
-var expressHandlebars  = require('express-handlebars');
-var url = require('url');
-var http = require('http');
-var https = require('https');
 var request = require('request');
-var progress = require('request-progress');
-var freesound = require('./lib/freesound.js');
 
 var server = express();
 
@@ -27,8 +21,6 @@ server.use('/apiv2', function(req, res) {
 
   req.pipe(r).pipe(res);
 });
-
-freesound.setToken(process.env.FREESOUND_API_KEY);
 
 var myPort = process.env.PORT || 3001;
 var mHost = process.env.VCAP_APP_HOST || "127.0.0.1";
