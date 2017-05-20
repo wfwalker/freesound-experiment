151f048fd2de4ad510d07bd5e3fc5c1f3390dc7e
Bill Walker <walker@shout.net>
2017-05-20 08:46:52 -0700

    add link to freesound web page

--- react-client/src/FreesoundDescription.js
+++ react-client/src/FreesoundDescription.js
@@ -6,6 +6,8 @@ function FreesoundDescription(props) {
       <img alt='waveform' height='20px' width='33px' src={props.data.images.waveform_m} />
       <a target='_blank' href={props.data.previews['preview-hq-mp3']}>
         <span className='timeLabel'>{Math.round(props.data.duration)}s</span>
+      </a>
+      <a target='_blank' href={props.data.url}>
         {props.data.name}
       </a>
     </div>
