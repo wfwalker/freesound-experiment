f54f0508ed0a36d22ecd3e21f1c78f262527f93e
Bill Walker <walker@shout.net>
2017-06-29 20:53:53 -0700

    make waveforms grayscale, inverted, higher contrast make Freesound (i. e., while playing) always 110px tall

--- react-client/src/FreesoundDescription.js
+++ react-client/src/FreesoundDescription.js
@@ -4,7 +4,7 @@ import './FreesoundDescription.css'
 function FreesoundDescription(props) {
   return (
     <div className='description'>
-      <img alt='waveform' height='20px' width='33px' src={props.data.images.waveform_m} />
+      <img alt='waveform' className='waveform' height='20px' width='33px' src={props.data.images.waveform_m} />
       <a target='_blank' href={props.data.previews['preview-hq-mp3']}>
         <div className='timeLabel'>{Math.round(props.data.duration)}s</div>
       </a>
