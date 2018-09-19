d7efffd68e4decee480f63fc0ed47d2c0293ba05
Bill Walker <walker@shout.net>
2018-09-18 23:29:08 -0700

    debug and hopefully fix backend host finder

--- react-client/src/api-config.js
+++ react-client/src/api-config.js
@@ -2,12 +2,12 @@ let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
 
-if(hostname === 'https://birdquiz.herokuapp.com/') {
-  backendHost = 'https://birdquiz.herokuapp.com/';
+if(hostname === 'freesound-experiment.herokuapp.com') {
+  backendHost = 'https://freesound-experiment.herokuapp.com/';
 } else {
   backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:3001';
 }
 
-console.log('THIS IS IT', backendHost);
+console.log('THIS IS IT', hostname, backendHost);
 
 export const API_ROOT = `${backendHost}`;
