235cb00f29d459c4be47ae722f60023c58f89928
Bill Walker <walker@shout.net>
2018-09-20 22:01:38 -0700

    try to observe existing protocol

--- react-client/src/api-config.js
+++ react-client/src/api-config.js
@@ -1,11 +1,12 @@
 let backendHost;
 
 const hostname = window && window.location && window.location.hostname;
+const requestProtocol = window.location.protocol;
 
 if(hostname === 'freesound-experiment.herokuapp.com') {
-  backendHost = 'http://freesound-experiment.herokuapp.com';
+  backendHost = requestProtocol + '//freesound-experiment.herokuapp.com';
 } else {
-  backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:3001';
+  backendHost = requestProtocol + (process.env.REACT_APP_BACKEND_HOST || '//localhost:3001');
 }
 
 console.log('THIS IS IT', hostname, backendHost);
