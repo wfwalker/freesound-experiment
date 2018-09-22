d63970a8f4faa9444c6628aee74e95c4cea94a27
Bill Walker <walker@shout.net>
2018-09-22 11:01:33 -0700

    go back to using relative URL's in production

--- react-client/src/api-config.js
+++ react-client/src/api-config.js
@@ -4,11 +4,11 @@ const hostname = window && window.location && window.location.hostname;
 const requestProtocol = window.location.protocol;
 
 if(hostname === 'freesound-experiment.herokuapp.com') {
-  backendHost = requestProtocol + '//freesound-experiment.herokuapp.com';
+  backendHost = '';
 } else {
   backendHost = requestProtocol + (process.env.REACT_APP_BACKEND_HOST || '//localhost:3001');
 }
 
-console.log('THIS IS IT', hostname, backendHost);
+console.log('API_ROOT from "', hostname, '" is "', backendHost, '"');
 
 export const API_ROOT = `${backendHost}`;
